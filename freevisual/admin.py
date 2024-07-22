ac0cffc1768767d5c570877cef6c9b5d56948c22
josemarimaya <josemarimaya@gmail.com>
2024-07-22 19:37:19 +0200

    few changes

--- freevisual/admin.py
+++ freevisual/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Creator)
+admin.site.register(models.Speciality)
+admin.site.register(models.Field)
+admin.site.register(models.Tools)
