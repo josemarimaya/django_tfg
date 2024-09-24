ab42596b90b5cf01a757395d83289612731c6749
josemarimaya <josemarimaya@gmail.com>
2024-09-24 10:35:18 +0200

    adding some confs for  images

--- mytfg/settings.py
+++ mytfg/settings.py
@@ -189,6 +189,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MEDIA_ROOT = '/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
